ed493b4aea94376f4bbb95f56c828b62286859da
ElenaQAE <fimushkin.dmitry@yandex.ru>
2022-12-24 11:40:41 +0300

    Create SelenideWirkiSearch#2

--- src/test/java/github/SelenideWikiSearch.java
+++ src/test/java/github/SelenideWikiSearch.java
@@ -1,12 +1,9 @@
 package github;
 
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
 public class SelenideWikiSearch {
  @Test
  void shouldFindJUnit5Text(){

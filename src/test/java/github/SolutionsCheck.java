196201f1c390d80bce19477b4b949e02e384e8bc
ElenaQAE <fimushkin.dmitry@yandex.ru>
2022-12-25 22:13:31 +0300

    Create SolutionsCheck (selenide hw part2)

---
+++ src/test/java/github/SolutionsCheck.java
@@ -0,0 +1,34 @@
+package github;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.CollectionCondition.*;
+
+public class SolutionsCheck {
+
+ @Test
+ void solutionPageCheck() {
+  open("https://github.com/");
+  // Переход в Solutions
+  $$(".d-lg-flex > li").get(1).hover(); // 1.вопрос: при работе со списками, нужно ли уточнять сперва, что это ul.класс или можно просто сразу написать .класс?
+  //$$("ul.d-lg-flex").first().click(); // 2. вопрос: почему при first нажимает на (Solutions), а не на (Product)?, он же первый идёт.
+  // $(".Header-old").$(byText("Solutions")).click(); // 3. вопрос: насколько хорошей практикой искать по тексту?
+  // Переход в Enterprise
+  $$(".d-lg-flex > li").get(1).$("a").click();
+  // Проверка заголовков на странице
+  $$("div>h1").shouldHave(texts("Build like the best", "Build like the best"));
+ }
+
+ @Test
+ void dragAndDropTriangleonHerokuapp() {
+  open("https://the-internet.herokuapp.com/drag_and_drop");
+  $(("#column-a")).dragAndDropTo($("#column-b"));
+  $$("#columns div").first().shouldHave(text("B"));
+  // попытки выполнить с actions
+  // actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+  //actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(350,0).release().perform();
+  // actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+ }
+}

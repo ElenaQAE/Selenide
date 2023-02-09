9f15ba8705adf38533fda319eef7ea3d699ce55b
ElenaQAE <fimushkin.dmitry@yandex.ru>
2023-02-09 17:55:17 +0300

    Added run on Selenoid №2 (added remote in TestBase)

--- src/test/java/jenkins_lesson_11/TestBase.java
+++ src/test/java/jenkins_lesson_11/TestBase.java
@@ -17,10 +17,10 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(

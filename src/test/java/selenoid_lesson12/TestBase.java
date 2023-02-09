e735fdb277fa7a29797373859c4362203c7d665e
ElenaQAE <fimushkin.dmitry@yandex.ru>
2023-02-09 18:06:39 +0300

    Added run on Selenoid №2 (added remote in TestBase)

--- src/test/java/selenoid_lesson12/TestBase.java
+++ src/test/java/selenoid_lesson12/TestBase.java
@@ -17,9 +17,9 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
         Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
